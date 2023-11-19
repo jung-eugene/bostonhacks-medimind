92aa5426ed47e03de73d0f3fe9813d8099f86c68
jung <eugenejung04@gmail.com>
2023-11-18 21:20:22 -0500

    update app.js with nav

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import DashboardScreen from './src/screens/DashboardScreen.js'; // Adjust the path according to your project structure
-import FoodRecommendation from './src/screens/FoodRecommendation.js';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { LoginScreen, SignupScreen } from './src/screens/AuthScreens.js'; // Ensure you export LoginScreen and SignupScreen from AuthScreens.js
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    // <DashboardScreen />
-    <FoodRecommendation />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
+}
 
 export default App;

1e9a5bfe2dfe2f9bf0aa18ed0f6cc64e2935c324
jung <eugenejung04@gmail.com>
2023-11-19 01:11:43 -0500

    delete unnecessary import

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './src/screens/StackNavigator.js'; 
-import Assessment1 from './src/screens/Assessment1.js';
 
 function App() {
   return (

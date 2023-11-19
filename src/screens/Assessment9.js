d63f90aafa9f8599e3c9cda91602f953a200c5e9
Eugene Jung <114817519+jung-eugene@users.noreply.github.com>
2023-11-19 02:05:46 -0500

    change bg color

--- src/screens/Assessment9.js
+++ src/screens/Assessment9.js
@@ -23,7 +23,7 @@ const Assessment9 = ( {navigation} ) => {
   );
 
   return (
-    <View>
+    <View style={styles.container}>
       <View style={styles.headerContainer}>
         <Text style={styles.header}>Assessment</Text>
         <View style={styles.numberContainer}>
@@ -46,6 +46,9 @@ const Assessment9 = ( {navigation} ) => {
 };
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+  },
   header: {
     marginTop: 60,
     marginLeft: 36,

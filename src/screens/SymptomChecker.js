c6a73558c5b73b3b1a47665c3b370e9c443e3672
jakejeong <jake50071227@gmail.com>
2023-11-19 03:27:14 -0500

    update

--- src/screens/SymptomChecker.js
+++ src/screens/SymptomChecker.js
@@ -1,10 +1,39 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native';
-import { Ionicons } from '@expo/vector-icons'; // Make sure you have @expo/vector-icons installed
-import { MaterialIcons } from '@expo/vector-icons';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native';
+import axios from 'axios';
+import { OPENAI_KEY } from '@env';
 
 const SymptomCheckerScreen = () => {
   const [symptom, setSymptom] = useState('');
+
+  const getGPTResponse = async () => {
+    if (!symptom.trim()) {
+      Alert.alert('Input Error', 'Please enter a symptom');
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        'https://api.openai.com/v1/engines/davinci-codex/completions',
+        {
+          prompt: `The following is a list of potential causes and remedies for the symptom: ${symptom}`,
+          max_tokens: 150,
+        },
+        {
+          headers: {
+            'Authorization': `Bearer ${OPENAI_KEY}`, // Replace with your actual OpenAI API key
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      // Handle the response as needed
+      Alert.alert('Possible Causes', response.data.choices[0].text);
+    } catch (error) {
+      console.error(error);
+      Alert.alert('API Error', 'An error occurred while trying to get a response from the API.');
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -14,31 +43,16 @@ const SymptomCheckerScreen = () => {
         powered by AI to help determine possible causes and treatments.
       </Text>
       
-      <View style={styles.searchSection}>
-        <Ionicons name="ios-search" size={20} color="#000" style={styles.searchIcon} />
-        <TextInput
-          style={styles.input}
-          placeholder="Type your symptom"
-          onChangeText={text => setSymptom(text)}
-          value={symptom}
-        />
-      </View>
+      <TextInput
+        style={styles.input}
+        placeholder="Type your symptom"
+        onChangeText={setSymptom}
+        value={symptom}
+      />
       
-      <TouchableOpacity style={styles.button} onPress={() => console.log('Show possible causes')}>
+      <TouchableOpacity style={styles.button} onPress={getGPTResponse}>
         <Text style={styles.buttonText}>Show possible causes</Text>
       </TouchableOpacity>
-
-      <View style={styles.navbarContainer}>
-          <TouchableOpacity style={styles.navItem}>
-              <MaterialIcons name="home" size={24} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.navItem} onPress={() => navigation.navigate('FoodRec')}>
-              <MaterialIcons name="restaurant-menu" size={24} color="black" />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.navItem} onPress={() => navigation.navigate('SymptomChecker')}>
-              <MaterialIcons name="calendar-today" size={24} color="black" />
-          </TouchableOpacity>
-      </View>
     </View>
   );
 };
@@ -46,15 +60,13 @@ const SymptomCheckerScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
     padding: 20,
+    backgroundColor: '#fff',
   },
   title: {
     fontSize: 22,
     fontWeight: 'bold',
-    marginTop: 20,
+    marginTop: 150,
     marginBottom: 10,
     textAlign: 'center',
   },
@@ -64,51 +76,26 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom: 20,
   },
-  searchSection: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  input: {
     borderWidth: 1,
-    borderColor: '#000',
-    padding: 10,
+    borderColor: 'grey',
     borderRadius: 5,
+    padding: 10,
+    fontSize: 18,
     marginBottom: 20,
   },
-  searchIcon: {
-    marginRight: 10,
-  },
-  input: {
-    flex: 1,
-    fontSize: 18,
-  },
   button: {
-    backgroundColor: '#799d00',
-    padding: 15,
+    backgroundColor: 'green',
     borderRadius: 5,
+    padding: 15,
     justifyContent: 'center',
     alignItems: 'center',
-    width: '100%',
+    elevation: 2,
   },
   buttonText: {
-    color: '#fff',
+    color: 'white',
     fontSize: 18,
-  },
-  navbarContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: '#ffffff',
-    borderTopWidth: 1,
-    borderTopColor: '#dddddd',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    height: 70,
-    paddingBottom: 10,
-  },
-  navItem: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontWeight: 'bold',
   },
 });
 

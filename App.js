f8eb66a4d99099458d43cce5b5016ac940de0cdf
jakejeong <jake50071227@gmail.com>
2023-11-19 05:02:42 -0500

    1

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import StackNavigator from './src/screens/StackNavigator.js'; 
+import ChatGPT35 from './src/screens/test.js';
+import ChatSymptomCheckerScreen from './src/screens/SymptomChecker.js';
 
 function App() {
   return (
--- src/screens/SymptomChecker.js
+++ src/screens/SymptomChecker.js
@@ -1,57 +1,72 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert, ScrollView } from 'react-native';
 import axios from 'axios';
-import { OPENAI_KEY } from '@env';
+import { OPENAI_KEY } from '@env'; // Make sure you have the react-native-dotenv package installed and configured
 
-const SymptomCheckerScreen = () => {
-  const [symptom, setSymptom] = useState('');
-
-  const getGPTResponse = async () => {
-    if (!symptom.trim()) {
-      Alert.alert('Input Error', 'Please enter a symptom');
-      return;
-    }
-
+const ChatSymptomCheckerScreen = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputText, setInputText] = useState('');
+  const sendMessage = async () => {
+    const userMessage = { role: 'user', content: inputText };
+  
+    // Update the messages state with the new user message
+    setMessages(prevMessages => [...prevMessages, userMessage]);
+  
+    setInputText('');
+  
     try {
-      const response = await axios.post(
-        'https://api.openai.com/v1/engines/davinci-codex/completions',
-        {
-          prompt: `The following is a list of potential causes and remedies for the symptom: ${symptom}`,
-          max_tokens: 150,
-        },
-        {
-          headers: {
-            'Authorization': `Bearer ${OPENAI_KEY}`, // Replace with your actual OpenAI API key
-            'Content-Type': 'application/json',
+      // Using a timeout to allow the state to update before making the API call
+      setTimeout(async () => {
+        const response = await axios.post(
+          'https://api.openai.com/v1/chat/completions',
+          {
+            model: 'gpt-3.5-turbo',
+            messages: [...messages, userMessage], // This may still be using the old state
           },
-        }
-      );
-
-      // Handle the response as needed
-      Alert.alert('Possible Causes', response.data.choices[0].text);
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${OPENAI_KEY}`,
+            },
+          }
+        );
+  
+        const botMessage = {
+          role: 'bot',
+          content: response.data.choices[0].message.content,
+        };
+  
+        // Update the messages state with the new bot message
+        setMessages(prevMessages => [...prevMessages, botMessage]);
+      }, 100);
     } catch (error) {
-      console.error(error);
-      Alert.alert('API Error', 'An error occurred while trying to get a response from the API.');
+      console.error('Error sending message:', error);
     }
   };
+  
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Symptom Checker</Text>
-      <Text style={styles.description}>
-        Experiencing symptoms but not sure what they mean? Use MediMind’s Symptom Checker
-        powered by AI to help determine possible causes and treatments.
-      </Text>
-      
+      <Text style={styles.title}>Chat with Symptom Checker</Text>
+
+      <ScrollView style={styles.messageContainer}>
+        {messages.map((message, index) => (
+          <Text key={index} style={[styles.message, message.role === 'user' ? styles.userMessage : styles.botMessage]}>
+            {message.content}
+          </Text>
+        ))}
+      </ScrollView>
+
       <TextInput
         style={styles.input}
-        placeholder="Type your symptom"
-        onChangeText={setSymptom}
-        value={symptom}
+        placeholder="Type your symptom or message"
+        onChangeText={setInputText}
+        value={inputText}
+        onSubmitEditing={sendMessage} // Send message when 'return' key is pressed
       />
-      
-      <TouchableOpacity style={styles.button} onPress={getGPTResponse}>
-        <Text style={styles.buttonText}>Show possible causes</Text>
+
+      <TouchableOpacity style={styles.button} onPress={sendMessage}>
+        <Text style={styles.buttonText}>Send</Text>
       </TouchableOpacity>
     </View>
   );
@@ -66,15 +81,28 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 22,
     fontWeight: 'bold',
-    marginTop: 150,
+    marginTop: 35,
     marginBottom: 10,
     textAlign: 'center',
   },
-  description: {
-    fontSize: 16,
-    color: 'grey',
-    textAlign: 'center',
+  messageContainer: {
+    flex: 1,
+    marginTop: 20,
     marginBottom: 20,
+  },
+  message: {
+    fontSize: 16,
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 10,
+  },
+  userMessage: {
+    alignSelf: 'flex-end',
+    backgroundColor: '#daf1da',
+  },
+  botMessage: {
+    alignSelf: 'flex-start',
+    backgroundColor: '#f1f1f1',
   },
   input: {
     borderWidth: 1,
@@ -82,7 +110,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     fontSize: 18,
-    marginBottom: 20,
+    marginBottom: 10,
   },
   button: {
     backgroundColor: 'green',
@@ -99,4 +127,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default SymptomCheckerScreen;
+export default ChatSymptomCheckerScreen;
